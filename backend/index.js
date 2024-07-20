f2c03d8a862a21966f0394c4c4d4ecc2f6294d6f
farhan294sha <farhanshabeer294@gmail.com>
2024-07-20 16:57:03 +0530

    signup endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
-import express from "express"
-const app = express()
-const port = 3000
+import express from "express";
+import bodyParser from "body-parser";
+import rootRouter from "./routes";
+import cors from "cors"
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/v1", rootRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
